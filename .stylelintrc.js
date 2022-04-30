fb4f6f802e065331496963586e7b881e6f102dd9
Brian Surowiec <brian@13degrees.com>
2022-04-29 22:58:08 -0400

    Initial commit

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  rules: {
+    "unit-allowed-list": [
+      ["vh"],
+      {
+        severity: "error",
+      },
+    ],
+    "color-named": [
+      "never",
+      {
+        severity: "warning",
+      },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Problem matcher test
+
+This repo demonstrates how problem matchers that match multiple severities do not work together.
